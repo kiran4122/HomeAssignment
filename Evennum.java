f517dd33226007e8860e8337b9559282de6d9fdc
kiran4122 <69193464+kiran4122@users.noreply.github.com>
2023-11-21 19:24:13 +0530

    Add files via upload

---
+++ Evennum.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class Evennum {
+
+	public static void main(String[] args) {
+		
+		int n = 100;
+		
+		for (int i = 1;i<=n;i++) {
+			
+			if (i%2==0) {
+				
+				System.out.println("the number is even "+ i);
+			}
+				
+				
+			
+			}
+		}
+
+	}
+
+
